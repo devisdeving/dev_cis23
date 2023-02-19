1af4b12c24fbb3ad39ef82dd719492749cd24298
devisdeving <makkd805@newschool.edu>
2023-02-18 19:39:05 -0500

    messed around w flare

--- projects/Stories_As_Networks/script.js
+++ projects/Stories_As_Networks/script.js
@@ -1,5 +1,16 @@
-('button').on('click', function(){
-    $('html, body').animate({
-        scrollTop: ('#foreword').offset().top
-    }, 1000)
-})
+var pointer = document.getElementById("pointer"),
+pointerBox = pointer.getBoundingClientRect(),
+centerPoint = window.getComputedStyle(pointer).transformOrigin,
+centers = centerPoint.split(" "),
+centerY = pointerBox.top + parseInt(centers[1]) - window.pageYOffset,
+centerX = pointerBox.left + parseInt(centers[0]) - window.pageXOffset;
+
+window.addEventListener("mousemove", function(e) {
+    var radians = Math.atan2(e.clientX - centerX, e.clientY - centerY);
+    var degree = (radians * (180 / Math.PI) * 1) + (180, 270); 
+    pointer.style.transform = "rotate("+degree+"deg)", {duration: 3000};
+    pointer.animate({
+        left:`${clientX}deg`,
+        top: `${clientY}deg`
+    }, {duration: 3000, fill: "forwards"})
+});

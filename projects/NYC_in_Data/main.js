531cd55a2c408fb473b5278a8f7db09fb9a97361
devisdeving <makkd805@newschool.edu>
2023-05-04 10:21:30 -0400

    loaded in json

--- projects/NYC_in_Data/main.js
+++ projects/NYC_in_Data/main.js
@@ -1,5 +1,5 @@
 // Function to render your items
-const collectionList = document.getElementById('collection')
+const collectionList = document.getElementById('set')
 
 function renderItems(collection) {
 	// The `ul` where the items will be inserted
@@ -8,21 +8,32 @@ function renderItems(collection) {
 	collection.forEach(function(item, index) {
 		const itemDetails =
 			`
-			<div class="card" class="${item.side}" id="${item.idName}" data-ownership='${item.ownership}' data-item-idx=${index}>
-			<h3 class="${item.class}">${item.placeName}</h3>	
-			<img src="${item.img}"/>
-			<div class="tags">
-				<p>Established in <time>${item.yearEstablished}</time></p>
-				<p><em>${item.crossLane}</em></p>
-				<p>${item.type}</p>
-				<p>${item.ownership}</p>
+			<div id="row">
+			<div id="div1">
+				<p>${item.houseNumber} ${item.streetName}</p>
 			</div>
-				</div>
-			
+			<div id="div2">
+				<p>${item.ratio}</p>
+			</div>
+			</div>
 			`;
 			collectionList.insertAdjacentHTML('beforeend', itemDetails); // Which can we then insert
 	});
 }
+
+// fetch('NYC_in_Data.json')
+//   .then(response => response.json())
+//   .then(data => {
+//     const ratio = data.ratio; // extract the ratio value from the JSON data
+    
+//     const container = document.getElementById("row"); // get the container element
+//     const div1 = document.getElementById("div1"); // get the first div element
+//     const div2 = document.getElementById("div2"); // get the second div element
+    
+//     // set the widths of the div elements based on the ratio
+//     div1.style.width = ratio * container.offsetWidth + "px";
+//     div2.style.width = (1 - ratio) * container.offsetWidth + "px";
+//   });
 
 fetch('NYC_in_Data.json')
 	.then(function(response) {
@@ -30,5 +41,7 @@ fetch('NYC_in_Data.json')
 	})
 	.then(function(collection){
 		// And passes the data to the function, above!
-		renderItems(collection) // In reverse order
+		renderItems(collection)
 	})
+
+	

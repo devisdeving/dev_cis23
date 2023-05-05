d8f1dc91d71e6ab271da15dbf9883ccabc636b22
devisdeving <makkd805@newschool.edu>
2023-05-05 11:52:08 -0400

    figuring out how to make code skip 0:0

--- projects/NYC_in_Data/main.js
+++ projects/NYC_in_Data/main.js
@@ -2,10 +2,16 @@ const collectionList = document.getElementById('set')
 
 function renderItems(collection) {
 	collection.forEach(function(item, index) {
+
+		if (item.ratio === "0:0") {
+			console.log("skip"); // Skip over this object and move to the next one
+		  }
+
 		const itemDetails =
 			`
 			<div id="row" style="height:${item.baseSquareFootage/20}px; width:${item.allCountedUnits * 50}px"; class="all">
 				<div class="rented" style="width:${item.countedRentalUnits * 50}px">
+				<p  class="rowText">${item.ratio}</p>
 				</div>
 				<div class="owned" style="width:${item.countedHomeownershipUnits * 50}px">
 				<p  class="rowText">${item.houseNumber} ${item.streetName}</p>
@@ -15,6 +21,7 @@ function renderItems(collection) {
 			collectionList.insertAdjacentHTML('beforeend', itemDetails); // Which can we then insert
 	});
 }
+
 
 fetch('NYC_in_Data.json')
 	.then(function(response) {

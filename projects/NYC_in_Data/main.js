26c800f39c7afc6228421a499bef50b566efc095
devisdeving <makkd805@newschool.edu>
2023-05-05 17:42:11 -0400

    empty working nnow

--- projects/NYC_in_Data/main.js
+++ projects/NYC_in_Data/main.js
@@ -2,24 +2,35 @@ const collectionList = document.getElementById('set')
 
 function renderItems(collection) {
 	collection.forEach(function(item, index) {
-
-		if (item.ratio === "0:0") {
-			console.log("skip"); // Skip over this object and move to the next one
-		  }
+		// if (item.countedHomeownershipUnits === 0) {
+		// 	return;
+		// } 
+		
+		// if (item.countedRentalUnits === 0) {
+		// 	return;
+		// }  
+		const emptyClass = item.allCountedUnits === 0 ? 'empty' : '';
 
 		const itemDetails =
 			`
-			<div id="row" style="height:${item.baseSquareFootage/20}px; width:${item.allCountedUnits * 50}px"; class="all">
-				<div class="rented" style="width:${item.countedRentalUnits * 50}px">
-				<p  class="rowText">${item.ratio}</p>
+			<div id="row"  class="${emptyClass}"
+			style="
+			height:${item.baseSquareFootage/20}px; 
+			width: 100vw;">
+			<!--<p  class="rowText">${item.ratio}</p>-->
+			<div class="rented" style="width:${item.countedRentalUnits / item.allCountedUnits * 100}%">
 				</div>
-				<div class="owned" style="width:${item.countedHomeownershipUnits * 50}px">
-				<p  class="rowText">${item.houseNumber} ${item.streetName}</p>
+				<div class="owned" style="width:${item.countedHomeownershipUnits / item.allCountedUnits * 100}%">
 				</div>
+				<!--<p  class="rowText">${item.houseNumber} ${item.streetName} ${item.postcode}</p>-->
+
 			</div>
 			`;
-			collectionList.insertAdjacentHTML('beforeend', itemDetails); // Which can we then insert
+			collectionList.insertAdjacentHTML('beforeend', itemDetails);
 	});
+
+	collection.forEach
+	
 }
 
 
@@ -31,4 +42,16 @@ fetch('NYC_in_Data.json')
 		renderItems(collection)
 	})
 
+	// <div id="row"
+	// style="
+	// height:${item.baseSquareFootage/20}px; 
+	// width: ${item.allCountedUnits * 50}px;">
+	// <p  class="rowText">${item.ratio}</p>
+	// <div class="rented" style="width:${item.countedRentalUnits / item.allCountedUnits * 100}%">
+	// 	</div>
+	// 	<div class="owned" style="width:${item.countedHomeownershipUnits * 50}px">
+	// 	</div>
+	// 	<p  class="rowText">${item.houseNumber} ${item.streetName} ${item.postcode}</p>
+
+	// </div>
 	

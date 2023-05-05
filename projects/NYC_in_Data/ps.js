9fbc06090529857c8547cc7413808f15330c98ac
devisdeving <makkd805@newschool.edu>
2023-05-05 11:03:35 -0400

    FIGURED OUT RATIOSSSSSSSSSSSS LFGGG

--- projects/NYC_in_Data/ps.js
+++ projects/NYC_in_Data/ps.js
@@ -4,12 +4,12 @@ function renderItems(collection) {
 	collection.forEach(function(item, index) {
 		const itemDetails =
 			`
-			<div id="row" style="height:${item.baseSquareFootage/20}px; width:${item.allCountedUnits * 50}px"; class="all">
-				<div class="rented" style="width:${item.countedRentalUnits * 50}px">
-				</div>
-				<div class="owned" style="width:${item.countedHomeownershipUnits * 50}px">
-				<p  class="rowText">${item.houseNumber} ${item.streetName}</p>
-				</div>
+			<div id="row">
+			<div style="height:${item.baseSquareFootage/100}px; width:${item.allCountedUnits * 75}%"; class="all">
+				<p>${item.houseNumber} ${item.streetName}</p>
+				<div class="rented" width:${item.countedRentalUnits * 50}px"></div>
+			</div>
+			<div class="owned" width:${item.countedHomeownershipUnits * 50}px"></div>
 			</div>
 			`;
 			collectionList.insertAdjacentHTML('beforeend', itemDetails); // Which can we then insert

ea81ef4edd17c95e174d6257ad0bb2b4d52072af
devisdeving <makkd805@newschool.edu>
2023-05-05 18:50:36 -0400

    animation sorted

--- projects/NYC_in_Data/main.js
+++ projects/NYC_in_Data/main.js
@@ -1,7 +1,37 @@
 const collectionList = document.getElementById('set')
 
+const rentedGradients = [
+	'linear-gradient(90deg, #000000, #ABC600)',
+	'linear-gradient(90deg, #000000, #EA6CFF)',
+	'linear-gradient(90deg, #000000, #00703a)',
+	'linear-gradient(90deg, #000000, #D1ACFF)',
+	'linear-gradient(90deg, #000000, #734132)',
+	'linear-gradient(90deg, #000000, #436cff)'
+  ];
+  const ownedGradients = [
+'linear-gradient(90deg, #000000, #ABC600)',
+'linear-gradient(90deg, #000000, #EA6CFF)',
+'linear-gradient(90deg, #000000, #00703a)',
+'linear-gradient(90deg, #000000, #D1ACFF)',
+'linear-gradient(90deg, #000000, #734132)',
+'linear-gradient(90deg, #000000, #436cff)'
+  ];
+  
+  let lastRentedGradient = null;
+  let lastOwnedGradient = null;
+  
+  function getRandomGradient(gradients, lastGradient) {
+	let gradient = lastGradient;
+	while (gradient === lastGradient) {
+	  gradient = gradients[Math.floor(Math.random() * gradients.length)];
+	}
+	return gradient;
+  }
+
 function renderItems(collection) {
 	collection.forEach(function(item, index) {
+	  const rentedGradient = getRandomGradient(rentedGradients);
+	  const ownedGradient = getRandomGradient(ownedGradients);
 		// if (item.countedHomeownershipUnits === 0) {
 		// 	return;
 		// } 
@@ -16,11 +46,13 @@ function renderItems(collection) {
 			<div id="row"  class="${emptyClass}"
 			style="
 			height:${item.baseSquareFootage/20}px; 
-			width: 100vw;">
+			width: 100vw;
+			--delay:${Math.random() * 2000}">
 			<!--<p  class="rowText">${item.ratio}</p>-->
-			<div class="rented" style="width:${item.countedRentalUnits / item.allCountedUnits * 100}%">
+			<div class="rowText">${item.houseNumber} ${item.streetName} ${item.postcode}</div>
+			<div class="rented" style="width:${item.countedRentalUnits / item.allCountedUnits * 100}%; background-image:${rentedGradient}">
 				</div>
-				<div class="owned" style="width:${item.countedHomeownershipUnits / item.allCountedUnits * 100}%">
+				<div class="owned" style="width:${item.countedHomeownershipUnits / item.allCountedUnits * 100}%; background-image:${ownedGradient}"">
 				</div>
 				<!--<p  class="rowText">${item.houseNumber} ${item.streetName} ${item.postcode}</p>-->
 
